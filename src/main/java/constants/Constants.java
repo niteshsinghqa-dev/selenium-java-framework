9eb4c51566a2d0f60acfe5eab49b0f2c67aa1760
nitesh <niteshsingh.qa@gmail.com>
2026-07-31 13:02:52 +0530

    Framework phase1

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,13 @@
+package constants;
+
+public class Constants {
+	  private Constants() {}
+
+	    public static final String CONFIG_FILE = "config.properties";
+
+	    public static final int IMPLICIT_WAIT = 10;
+
+	    public static final int EXPLICIT_WAIT = 20;
+
+	    public static final int PAGE_LOAD_TIMEOUT = 30;
+}
